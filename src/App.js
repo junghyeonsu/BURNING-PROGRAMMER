1321df0874f580b7e2efc608da6cd926c28693e5
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-07 00:08:00 +0900

    Grid 시스템 적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+/* Grid 관련 참고사이트들 
+1. https://material-ui.com/components/grid/  (공식사이트)
+2. https://jongbeom-dev.tistory.com/35       (참고블로그)
+*/
+
+
+class App extends Component {
+  render(){
+    const { classes } = this.props;
+    return(
+      <div className="App">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.paper}>Content 1</Paper>
+            </Grid>
+          </Grid>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+const styles = theme => ({
+  root: {
+    padding: theme.spacing(3),
+    background: '#eeeeee',
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+  },
+});
+
+export default withStyles(styles)(App);
