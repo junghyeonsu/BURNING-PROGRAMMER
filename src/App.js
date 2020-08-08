7dc4670abd6e45e0fe77cf2c7c9b276249437e37
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-08 14:42:22 +0900

    main homepage ui
    made ui

--- src/App.js
+++ src/App.js
@@ -1,90 +1,47 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
 /* Grid 관련 참고사이트들 
-1. https://material-ui.com/components/grid/  (공식사이트)
-2. https://jongbeom-dev.tistory.com/35       (참고블로그)
+1. https://studiomeal.com/archives/533     (참고블로그)
 */
 
+import Content from './components/Content';
 
 class App extends Component {
   render(){
-    const { classes } = this.props;
     return(
       <div className="App">
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>Content 1</Paper>
-            </Grid>
-          </Grid>
+        <div className="container-main">
+          <div className="item-main">제목</div>
+          
+          <div className="item">
+            <div className="container-content">
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              <div className="item-content">IMAGE</div>
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-
-const styles = theme => ({
-  root: {
-    padding: theme.spacing(3),
-    background: '#eeeeee',
-  },
-  paper: {
-    padding: theme.spacing(3),
-    textAlign: 'center',
-    color: theme.palette.text.primary,
-  },
-});
-
-export default withStyles(styles)(App);
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../styles/Content.css' 
+
+class Content extends Component {
+    render(){
+        return(
+            <div>
+                hi
+            </div>
+        );
+    }
+}
+
+export default Content;
