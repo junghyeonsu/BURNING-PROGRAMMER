07ce979e7ba50891563e6c4ef0a60c03fa6176d9
easyone <thinkanddoit@naver.com>
2020-08-09 19:43:39 +0900

    side bar 추가
    side bar 추가
    #css 수정해야 해야함

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,41 @@
-import React, {Component} from 'react';
-import '../styles/Sidebar.css' 
+import "../styles/Sidebar.css";
+import React from "react";
 
-class Sidebar extends Component {
-    render(){
-        return(
-            <div>
-              sidebar
-            </div>
-        );
+export const Sidebar = ({ width, height, children }) => {
+  const [xPosition, setX] = React.useState(-width);
+
+  const toggleMenu = () => {
+    if (xPosition < 0) {
+      setX(0);
+    } else {
+      setX(-width);
     }
-}
+  };
+
+  React.useEffect(() => {
+    setX(0);
+  }, []);
+  return (
+    <React.Fragment>
+      <div
+        className="side-bar"
+        style={{
+          transform: `translatex(${xPosition}px)`,
+          width: width,
+          minHeight: height
+        }}
+      >
+        <button
+          onClick={() => toggleMenu()}
+          className="toggle-menu"
+          style={{
+            transform: `translate(${width}px, 20vh)`
+          }}
+        ></button>
+        <div className="content">{children}</div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Sidebar;
