991079b6a7f17af366c8ff7b0f0c0ec7078f78c5
jerry <kskyung0624@gmail.com>
2020-08-12 03:26:51 +0900

    imported images

--- src/App.js
+++ src/App.js
@@ -7,15 +7,44 @@ import FireEffect from './components/FireEffect';
 import ToggleButton from './components/ToggleButton';
 import SidebarButton from './components/SidebarButton';
 
-import pythonImg from './images/python.png';
+import dotNetImg from './images/.Net.png';
+import AngularJSImg from './images/AngularJS.png'
 import arduinoImg from './images/arduino.png';
+import BootstrapImg from './images/Bootstrap.png';
 import cPlusPlusImg from './images/C++.png';
+import ClojureImg from './images/Clojure.png';
+import CSSImg from './images/CSS.png';
+import djangoImg from './images/django.png';
+import ElixirImg from './images/Elixir.png';
+import FalconImg from './images/Falcon.png';
+import FlaskImg from './images/Flask.png';
+import HaskellImg from './images/Haskell.png';
+import HTMLImg from './images/HTML.png';
 import javaImg from './images/java.png';
+import javaScriptImg from './images/JavaScript.png';
 import juliaImg from './images/julia.png';
+import kotlinImg from './images/kotlin_image.png';
+import LaravelImg from './images/Laravel.png';
 import lispImg from './images/Lisp.png';
+import MySQLImg from './images/MySQL.png';
+import NodeDotJSImg from './images/Node.js.png';
+import PHPImg from './images/PHP.png';
+import PostgreSQLImg from './images/PostgreSQL.png'
 import prologImg from './images/prolog.png';
+import pythonImg from './images/python.png';
 import rImg from './images/R_Programming.png';
 import raspberryPiImg from './images/RaspberryPi.png';
+import ReactImg from './images/React.png';
+import react_nativeImg from './images/react-native_image.png';
+import RubyImg from './images/Ruby.png';
+import RubyOnRailsImg from './images/Ruby on Rails.png';
+import SanicImg from './images/Sanic.png';
+import ScalaImg from './images/Scala.png';
+import SwiftImg from './images/swift_image.png';
+import TornadoImg from './images/Tornado.png';
+import UnityImg from './images/Unity.png';
+import ViboraImg from './images/Vibora.png';
+import VueDotJSImg from './images/Vue.js.png';
 
 import { Transition, TransitionGroup } from "react-transition-group";
 
