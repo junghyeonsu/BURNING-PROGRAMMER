d4a030ea440a2f7cfd6207fd41d63f67beb8d684
jerry <kskyung0624@gmail.com>
2020-08-12 12:30:52 +0900

    add categories & buttons

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import dotNetImg from './images/.Net.png';
 import AngularJSImg from './images/AngularJS.png'
 import arduinoImg from './images/arduino.png';
 import BootstrapImg from './images/Bootstrap.png';
+import CImg from './images/C.png'
+import CsharpImg from './images/Csharp.png'
 import cPlusPlusImg from './images/C++.png';
 import ClojureImg from './images/Clojure.png';
 import CSSImg from './images/CSS.png';
@@ -18,6 +20,7 @@ import djangoImg from './images/django.png';
 import ElixirImg from './images/Elixir.png';
 import FalconImg from './images/Falcon.png';
 import FlaskImg from './images/Flask.png';
+import GoImg from './images/Go.png'
 import HaskellImg from './images/Haskell.png';
 import HTMLImg from './images/HTML.png';
 import javaImg from './images/java.png';
@@ -53,16 +56,49 @@ import { Transition, TransitionGroup } from "react-transition-group";
 
 /* 프로그래밍 언어 리스트 */
 /* 순서 = 카테고리 배열(all = default), 이미지, 텍스트 */
+// 처리 안 한 것 : 자바 혹은 java로 설정
 const language_list = [
-  [["all", "자바"], javaImg, "Java"],
-  [["all", "web"], pythonImg, "Python"],
-  [["all", "자바"], cPlusPlusImg, "C++"],
-  [["all", "c++"], arduinoImg, "Arduino"],
-  [["all", "web"], juliaImg, "Julia"],
-  [["all", "자바"], prologImg, "Prolog"],
-  [["all", "자바"], lispImg, "Lisp"],
-  [["all", "java"], rImg, "R"],
-  [["all", "java"], raspberryPiImg, "Raspberry Pi"],
+  [["all", "웹"], dotNetImg, ".Net"],
+  [["all", "웹"], AngularJSImg, "AngularJS"],
+  [["all", "아두이노 / 라즈베리파이"], arduinoImg, "Arduino"],
+  [["all", "웹"], BootstrapImg, "Bootstrap"],
+  [["all", "보안"], CImg, "C"],
+  [["all", "블록체인"], CsharpImg, "C#"],
+  [["all", "게임", "블록체인", "보안", "머신러닝"], cPlusPlusImg, "C++"],
+  [["all", "자바"], ClojureImg, "Clojure"],
+  [["all", "웹"], CSSImg, "CSS"],
+  [["all", "웹"], djangoImg, "Django"],
+  [["all", "자바"], ElixirImg, "Elixir"],
+  [["all", "자바"], FalconImg, "Falcon"],
+  [["all", "웹"], FlaskImg, "Flask"],
+  [["all", "블록체인"], GoImg, "Go"],
+  [["all", "자바"], HaskellImg, "Haskell"],
+  [["all", "웹"], HTMLImg, "HTML"],
+  [["all", "모바일", "머신러닝"], javaImg, "Java"],
+  [["all", "웹", "블록체인"], javaScriptImg, "JavaScript"],
+  [["all", "머신러닝"], juliaImg, "Julia"],
+  [["all", "모바일"], kotlinImg, "Kotlin"],
+  [["all", "모바일"], LaravelImg, "Laravel"],
+  [["all", "머신러닝"], lispImg, "Lisp"],
+  [["all", "데이터 베이스"], MySQLImg, "MySQL"],
+  [["all", "웹"], NodeDotJSImg, "Node.JS"],
+  [["all", "웹"], PHPImg, "PHP"],
+  [["all", "데이터 베이스"], PostgreSQLImg, "PostgreSQL"],
+  [["all", "머신러닝"], prologImg, "Prolog"],
+  [["all", "머신러닝", "웹", "보안", "블록체인"], pythonImg, "Python"],
+  [["all", "머신러닝"], rImg, "R"],
+  [["all", "아두이노 / 라즈베리파이"], raspberryPiImg, "Raspberry Pi"],
+  [["all", "웹"], ReactImg, "React"],
+  [["all", "모바일"], react_nativeImg, "React-Native"],
+  [["all", "웹"], RubyImg, "Ruby"],
+  [["all", "웹"], RubyOnRailsImg, "Ruby On Rail"],
+  [["all", "웹"], SanicImg, "Sanic"],
+  [["all", "java"], ScalaImg, "Scala"],
+  [["all", "모바일"], SwiftImg, "Swift"],
+  [["all", "웹"], TornadoImg, "Tornado"],
+  [["all", "게임"], UnityImg, "Unity"],
+  [["all", "웹"], ViboraImg, "Vibora"],
+  [["all", "웹"], VueDotJSImg, "Vue.JS"],
 ];
 
 class App extends Component {
@@ -97,6 +133,18 @@ class App extends Component {
           <Sidebar width={200} height={"100vh"}>
               <h1>카테고리</h1>
               <SidebarButton click={this.OnClickSidebarButton}>자바</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>웹</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>모바일</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>데이터 베이스</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>클라우드</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>머신러닝</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>아두이노 / 라즈베리파이</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>블록체인</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>보안</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>게임</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>빅데이터</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>로봇</SidebarButton>
+              <SidebarButton click={this.OnClickSidebarButton}>통신</SidebarButton>
             </Sidebar>
         </div>
         <div className="container-main">
