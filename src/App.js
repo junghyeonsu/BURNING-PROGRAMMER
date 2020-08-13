a92d96b042cca789361f78397d5e2fb95d38c311
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-13 21:44:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { inject, observer } from 'mobx-react';
 
-import ContentContainer from './components/Content/ContentContainer';
-import Sidebar from './components/Sidebar/Sidebar';
-import HeaderText from './components/Header/HeaderText';
+import ContentContainer from './components/ContentContainer';
+import Sidebar from './components/Sidebar';
+import HeaderText from './components/HeaderText';
 
 import { Transition, TransitionGroup } from "react-transition-group";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 
 // https://www.npmjs.com/package/react-animation-components 참고할거임.
+
+import Container from './components/ContentContainer';
+import Python from './components/Python/Python';
 
 @inject('store')
 @observer
 class App extends Component {
    render() {
     return (
-      <div className="App">
-        <div className="sidebar">
-          <Sidebar width={200} height={"100vh"} />
+      <Router>
+
+        <div className="item-main">
+          <HeaderText />
+          <p>{!this.state.view ? <Link onClick={this.onClickhandler} to="/">홈으로</Link>: ""}</p>
         </div>
-        <div className="container-main">
-          <div className="item-main">
-            <HeaderText />
+
+        {this.state.view ?
+        <div className="App">
+          <div className="sidebar">
+            <Sidebar width={200} height={"100vh"} />
           </div>
-            <div>
-              <ContentContainer />
+          <div className="container-main">
+            <div className="container-content">
+              <ContentContainer onClick={this.onClickhandler} />
+            </div>
           </div>
         </div>
-      </div>
+        
+        :""}
+        
+        <Switch>
+          <Route exact path="/python" component={Python}/>
+        </Switch>
+      </Router>
+
     );
   }
 }
